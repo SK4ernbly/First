31cbe268348cd41c753d85f3998ac57a259631ce
Kolomoets Sergey <sk4ernbly@gmail.com>
2014-05-15 15:58:03 +0300

    copy DB from Assets working_variant

--- Notes/app/src/main/java/my/android/notes/app/db/AdapterDB.java
+++ Notes/app/src/main/java/my/android/notes/app/db/AdapterDB.java
@@ -4,6 +4,13 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import my.android.notes.app.enums.OperationType;
 
@@ -11,9 +18,6 @@ import my.android.notes.app.enums.OperationType;
  * Created by Olya on 10/04/14.
  */
 public class AdapterDB {
-
-    public static final String DB_NAME = "money.db";
-    public static final int DB_VERSION = 1;
 
     private DbHelper dbHelper;
     private Context context;
@@ -30,7 +34,7 @@ public class AdapterDB {
 
 
 
-    public AdapterDB(Context context){
+    public AdapterDB(Context context) throws IOException {
         this.context = context;
         dbHelper = new DbHelper(context);
         db = dbHelper.getReadableDatabase();
@@ -64,25 +68,69 @@ public class AdapterDB {
         }
 
         return c;
-
     }
 
 
     private static class DbHelper extends SQLiteOpenHelper {
 
-        public DbHelper(Context context) {
+        public static final String DB_NAME = "money.db";
+        public static final int DB_VERSION = 1;
+        private Context context;
+        private String dbPath;
+
+        public DbHelper(Context context) throws IOException {
             super(context, DB_NAME, null, DB_VERSION);
+            this.context = context;
+            dbPath = context.getApplicationInfo().dataDir + "/databases/" + DB_NAME;
+
+            if(checkDataBase()){
+            }
+            else{
+                this.getReadableDatabase();
+                copyDataBase();
+            }
+        }
+
+        private boolean checkDataBase(){
+            File dbFile = new File(dbPath);
+            return dbFile.exists();
+        }
+
+        private void copyDataBase() {
+            InputStream myInput = null;
+            OutputStream myOutput = null;
+            try {
+                myInput = context.getAssets().open(DB_NAME);
+                myOutput = new FileOutputStream(dbPath);
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = myInput.read(buffer)) > 0) {
+                    myOutput.write(buffer, 0, length);
+                }
+
+            } catch (Exception e) {
+                Log.e(getClass().getName(), e.getMessage());
+            } finally {
+                try {
+                    myOutput.flush();
+                    myOutput.close();
+                    myInput.close();
+                } catch (IOException e) {
+                    Log.e(getClass().getName(), e.getMessage());
+                }
+            }
 
         }
 
+
+
+
         @Override
         public void onCreate(SQLiteDatabase db) {
-
         }
 
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
         }
 
     }
