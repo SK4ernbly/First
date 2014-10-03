0a761c0bc5652235e67cd06be5a6f444139e8258
Kolomoets Sergey <sk4ernbly@gmail.com>
2014-10-03 16:53:52 +0300

    Animations_StartWithXmlPullParser_03102014

--- Notes/app/src/main/java/my/android/notes/app/activity/Weather.java
+++ Notes/app/src/main/java/my/android/notes/app/activity/Weather.java
@@ -1,18 +1,50 @@
 package my.android.notes.app.activity;
 
 import android.app.Activity;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 import my.android.notes.app.R;
 
 public class Weather extends Activity {
+
+    private Button checkWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         overridePendingTransition(R.anim.weather_transl, R.anim.weather_alpha);
+        checkWeather = (Button) findViewById(R.id.button_check);
+        checkWeather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    loadWeatherInfo();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (XmlPullParserException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
     }
 
@@ -40,5 +72,28 @@ public class Weather extends Activity {
     public void onBackPressed() {
         super.onBackPressed();
         overridePendingTransition(R.anim.weather_transl, R.anim.weather_alpha);
+    }
+
+    private void loadWeatherInfo() throws IOException, XmlPullParserException {
+        URL url = new URL("http://export.yandex.ru/weather-ng/forecasts/33372.xml");
+        XmlPullParser xpp =  XmlPullParserFactory.newInstance().newPullParser();
+        xpp.setInput(url.openConnection().getInputStream(), null);
+        while(xpp.getEventType() != XmlPullParser.END_DOCUMENT){
+            if(xpp.getEventType() == XmlPullParser.START_TAG && xpp.getName().equals("day date")){
+
+            }
+        }
+
+
+
+    }
+
+
+    private boolean isConnect(){
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo netwInfo = connManager.getActiveNetworkInfo();
+        if(netwInfo != null && netwInfo.isConnected())
+            return true;
+        else return false;
     }
 }
